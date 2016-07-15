bcbf6c9ad03d49bbc3877ab3aff30bf1566b5fff
Kwang Kim <kkim@ondeck.com>
2016-07-15 15:26:43 -0400

    added list/ls as commands to print tasks

--- blncd.js
+++ blncd.js
@@ -57,7 +57,7 @@ if (ask == 'create' || !apiKey) {
 		if (res.statusCode !== 200 && res.statusCode !== 201) {
 			console.log(res.body.error);
 		} else {
-			if (ask.indexOf('get') >= 0) {
+			if (ask.indexOf('get') >= 0 || ask.indexOf('list') >= 0 || ask.indexOf('ls') >= 0) {
 				newline();
 				var quadrant;
 				var filter = function(task) {
